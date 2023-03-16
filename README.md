479eafc9c08eb12b6be19773607d87a832a15a05
FlyingThaCat <2021071031@student.pppkpetra.sch.id>
2023-03-17 06:44:08 +0700

    Test Latest

--- README.md
+++ README.md
@@ -3,16 +3,10 @@ idk im just dumb w/ bash
 
 this script supposedly scan the rndis interface and nmap it to find where the ip has ssh open
 
-
-# BUG TERMUX FIRST TIME
-## DO PKG UPDATE AND UPGRADE 
-```sh
-pkg update -y && pkg upgrade -y
-
-#just enter when it ask default Y/N/blah
-```
-
 # USAGE
 ```sh
-pkg install wget -y && wget https://raw.githubusercontent.com/FlyingThaCat/IPscan/main/runs.sh && chmod +x runs.sh && ./runs.sh
+curl -o- https://raw.githubusercontent.com/FlyingThaCat/IPscan/main/runs.sh | bash
+```
+```sh
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 ```
--- runs.sh
+++ runs.sh
@@ -2,73 +2,147 @@
 # IP & PORT SCANNER FOR RNDIS INTERFACE 
 # @Flyingthacat (github)
 
+# COLORS
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-check_termux() {
-  echo $PREFIX | grep -q "com.termux"
-}
+# EMPTY VARIABLES
+PHONE_IP=""
+PHONE_NETMASK=""
+NETWORK_ADDRESS=""
+INTERFACE=""
+COUNTER=0
 
-if check_termux; then
+# CHECK IF RUNNING ON TERMUX
+if echo $PREFIX | grep -q "com.termux"; then
     printf "${GREEN}Running On Termux ...${NC}\n"
-  else
+else
     printf "${RED}Sorry This Script Only Works On Termux ...${NC}\n"
     exit
 fi
 
+# Update Termux
+printf "${GREEN}Updating Termux ...${NC}\n"
+apt-get update -y -q > /dev/null
 
-printf "${GREEN}Update And Upgrading Termux ...${NC}\n"
-pkg update -y -q && pkg upgrade -y -q
+# Upgrade Termux
+printf "${GREEN}Upgrading Termux ...${NC}\n"
+apt-get upgrade -y -q -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" > /dev/null
 
+# Function To Check If Package Is Installed
+check_package(){
+  if ! command -v $1 &> /dev/null
+  then 
+    printf "${RED}$1 Not Installed ...${NC}\n"
+    printf "${BLUE}Do You Want To Install $1 ... (y/n)${NC}\n"
+    read answer
+    if [ "$answer" != "${answer#[Yy]}" ] ;then
+      apt-get install $1 -y > /dev/null
+    else
+      exit
+    fi
+  fi
+}
+
+# Check If Nmap Is Installed
 printf "${GREEN}Checking Nmap ...${NC}\n"
+check_package nmap
 
-if ! command -v nmap -V &> /dev/null
-then 
-  printf "${RED}Nmap Not Installed ...${NC}\n"
-  printf "${BLUE}Do You Want To Install Nmap ... (y/n)${NC}\n"
-  read answerNmap
-  if [ "$answerNmap" != "${answerNmap#[Yy]}" ] ;then
-    pkg install nmap -y
-  else
-    exit
-  fi
-fi
-
+# Check If Ipcalc Is Installed
 printf "${GREEN}Checking IPCalc ...${NC}\n"
+check_package ipcalc
 
-if ! command -v ipcalc -v &> /dev/null
-then 
-  printf "${RED}Ipcalc Not Installed ...${NC}\n"
-  printf "${BLUE}Do You Want To Install Ipcalc ... (y/n)${NC}\n"
-  read answerIpcalc
-  if [ "$answerIpcalc" != "${answerIpcalc#[Yy]}" ] ;then
-    pkg install ipcalc -y
-  else
+# Make Function To Check Interface
+check_interface(){
+  if ! ifconfig 2> /dev/null | grep -q "$1"
+  then
+    printf "${RED}$1 Interface Not Found !!!!!${NC}\n"
     exit
   fi
+}
+
+# Make Function To Get Ip Address From Interface
+get_ip(){
+  ifconfig 2> /dev/null | grep -A 1 "$1" | awk '/inet /{print $2}'
+}
+
+# Make Function To Get Netmask From Interface
+get_netmask(){
+  ifconfig 2> /dev/null | grep -A 1 "$1" | awk '/netmask/{print $4}'
+}
+
+# if not match rndis or usb0 do something
+if ! ifconfig 2> /dev/null | grep -q "rndis\|usb0"
+then
+  printf "${RED}No Interface Found !!!!!${NC}\n"
+
+  printf "${RED} It Seems Your Interface Is Not Rndis Or Usb0 ... Trying The Manual Method${NC}\n"
+  
+  # printf debug from ifconfig
+  ifconfig 2> /dev/null
+
+  printf "${BLUE}Enter Your Interface Name For Usb Tethering : ${NC}\n"
+  read interface
+
+  # Check If Interface Is Found
+  printf "${GREEN}Checking Interface ...${NC}\n"
+  if ! ifconfig 2> /dev/null | grep -q "$interface"
+  then
+    printf "${RED}$interface Interface Not Found !!, You must be Entering A Wrong Interface Name${NC}\n"
+    exit
+  fi  
+
+  # set interface to variable
+  INTERFACE=$interface
 fi
 
-if ! ifconfig 2> /dev/null | grep -q "rndis"
+# Check If Rndis Interface Is Found
+printf "${GREEN}Checking Rndis Interface ...${NC}\n"
+if check_interface rndis
 then
-  printf "${RED}Usb Tethering Interface Not Found !!!!!${NC}\n"
+  INTERFACE="rndis"
+fi
+
+# Check If Usb0 Interface Is Found
+printf "${GREEN}Checking Usb0 Interface ...${NC}\n"
+if check_interface usb0
+then
+  INTERFACE="usb0"
+fi
+
+# if interface empty exit
+if [ -z "$INTERFACE" ]
+then
+  printf "${RED}Interface Variable Is Empty !!!!!${NC}\n"
   exit
 fi
 
-printf "${GREEN}Checking Phone IP On Rndis ...${NC}\n"
-PHONE_IP_RNDIS=$(ifconfig 2> /dev/null | grep -A 1 "rndis" | awk '/inet /{print $2}')
-printf "${BLUE}Phone IP : ${PHONE_IP_RNDIS}${NC}\n"
+# Get Phone Ip Address
+printf "${GREEN}Checking Phone IP On $INTERFACE ...${NC}\n"
+PHONE_IP=$(get_ip $INTERFACE)
+printf "${BLUE}Phone IP : ${PHONE_IP}${NC}\n"
 
-printf "${GREEN}Checking Phone Netmask On Rndis ...${NC}\n"
-PHONE_NETMASK_RNDIS=$(ifconfig 2> /dev/null | grep -A 1 "rndis" | awk '/netmask/{print $4}')
-printf "${BLUE}Phone Netmask : ${PHONE_NETMASK_RNDIS}${NC}\n"
+# Get Phone Netmask
+printf "${GREEN}Checking Phone Netmask On $INTERFACE ...${NC}\n"
+PHONE_NETMASK=$(get_netmask $INTERFACE)
+printf "${BLUE}Phone Netmask : ${PHONE_NETMASK}${NC}\n"
 
-NETWORK_ADDRESS=$(ipcalc $PHONE_IP_RNDIS | awk '/Network:/ { print $2 }')
-printf "${BLUE}Founded Network Address : ${NETWORK_ADDRESS}${NC}\n"
+# Get Network Address
+printf "${GREEN}Checking Network Address ...${NC}\n"
+NETWORK_ADDRESS=$(ipcalc $PHONE_IP $PHONE_NETMASK | awk '/Network:/ { print $2 }')
 
+# if network address empty exit
+if [ -z "$NETWORK_ADDRESS" ]
+then
+  printf "${RED}Network Address Variable Is Empty !!!!!${NC}\n"
+  exit
+fi
+printf "${BLUE}Network Address : ${NETWORK_ADDRESS}${NC}\n"
+
+# Do Nmap Scan
 printf "${GREEN}Do Nmap Scan ... ${NC}\n"
-counter=0
 
 nmap_scan(){
   nmap -p 22 --open -oG - $NETWORK_ADDRESS | awk '/Up$/{print $2}'
